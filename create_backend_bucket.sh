cdf660c390d7230154dcc665b7dc66cb2ca46e82
Amanda <mandacosta94@gmail.com>
2025-08-31 16:19:47 -0300

    update create_backend_bucket.sh

--- create_backend_bucket.sh
+++ create_backend_bucket.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Variáveis
 REGION="us-east-1"
-BUCKET_NAME="s3-golunch-infra-amanda"
+BUCKET_NAME="s3-golunch-infra"
 
 # Verifica se o bucket já existe
 if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
