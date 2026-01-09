4eccba5a78353b178030943a1bfcf5eb384741e3
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2026-01-09 14:15:42 -0300

    fix: adjust shell file to generate a bucket name based in account id

--- create_backend_bucket.sh
+++ create_backend_bucket.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 
-# Variáveis
 REGION="us-east-1"
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-BUCKET_NAME="s3-golunch-infra-terraform-fiap"
+BUCKET_NAME="s3-golunch-infra-terraform-${ACCOUNT_ID}"
 
-# Verifica se o bucket já existe
+echo "🔍 Verificando bucket: $BUCKET_NAME"
+
 if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
-  echo "✅ Bucket $BUCKET_NAME já existe."
+  echo "✅ Bucket $BUCKET_NAME já existe e pertence a esta conta."
 else
   echo "🚀 Criando bucket $BUCKET_NAME em $REGION..."
 
   if [ "$REGION" = "us-east-1" ]; then
     aws s3api create-bucket \
-      --bucket "$BUCKET_NAME" \
-      --region "$REGION"
+      --bucket "$BUCKET_NAME"
   else
     aws s3api create-bucket \
       --bucket "$BUCKET_NAME" \
-      --region "$REGION" \
       --create-bucket-configuration LocationConstraint="$REGION"
   fi
 
-  echo "🔄 Ativando versionamento no bucket $BUCKET_NAME..."
+  echo "🔄 Ativando versionamento..."
   aws s3api put-bucket-versioning \
     --bucket "$BUCKET_NAME" \
     --versioning-configuration Status=Enabled
 
-  echo "✅ Bucket $BUCKET_NAME criado com versionamento habilitado."
+  echo "✅ Bucket criado com sucesso."
 fi
