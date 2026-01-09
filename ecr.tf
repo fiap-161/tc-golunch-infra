ba01d128c81248611092081431339426520f7066
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2026-01-09 14:11:17 -0300

    fix format of ecr

--- ecr.tf
+++ ecr.tf
@@ -3,46 +3,43 @@ resource "aws_ecr_repository" "golunch" {
 }
 
 resource "aws_ecr_repository" "golunch_core_service" {
-  name = "golunch-core-service"
-  
+  name                 = "golunch-core-service"
   image_tag_mutability = "MUTABLE"
-  
+
   image_scanning_configuration {
     scan_on_push = true
   }
-  
+
   tags = {
-    Name = "golunch-core-service"
+    Name    = "golunch-core-service"
     Service = "core"
   }
 }
 
 resource "aws_ecr_repository" "golunch_payment_service" {
-  name = "golunch-payment-service"
-  
+  name                 = "golunch-payment-service"
   image_tag_mutability = "MUTABLE"
-  
+
   image_scanning_configuration {
     scan_on_push = true
   }
-  
+
   tags = {
-    Name = "golunch-payment-service"
+    Name    = "golunch-payment-service"
     Service = "payment"
   }
 }
 
 resource "aws_ecr_repository" "golunch_operation_service" {
-  name = "golunch-operation-service"
-  
+  name                 = "golunch-operation-service"
   image_tag_mutability = "MUTABLE"
-  
+
   image_scanning_configuration {
     scan_on_push = true
   }
-  
+
   tags = {
-    Name = "golunch-operation-service"
+    Name    = "golunch-operation-service"
     Service = "operation"
   }
 }
